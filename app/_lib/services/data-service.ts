5a3e92f5844c1a0742e72a4a2e597caf009f7011
Wilhelm Buchmann <buchmannwilhelm23@gmail.com>
2025-01-01 14:55:28 +0100

    changed getAppUser to return error:

--- app/_lib/services/data-service.ts
+++ app/_lib/services/data-service.ts
@@ -7,7 +7,10 @@ type AppUser = Database["public"]["Tables"]["app_users"]["Row"];
 export async function getAppUser(email: string) {
  
     const {data, error} = await supabase.from("app_users").select("*").eq("email", email).single();
-  
+    if (error) {
+        console.error(error);
+        throw new Error("User could not be found");
+      }
     return data;
 }
 
